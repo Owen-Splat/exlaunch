d381fc9af7fbd9f54a98758a878c9d8e80f8a279
Owen_Splat <Owen32Smart@gmail.com>
2026-03-16 12:04:06 -0400

    add support for playing bgm

--- source/program/Hooks/Common/audio.cpp
+++ source/program/Hooks/Common/audio.cpp
@@ -1,6 +1,16 @@
 #include "../lib.hpp"
 #include <string>
 
+long unkPtr = 0;
+HOOK_DEFINE_TRAMPOLINE(AudioSystem__PlayBGM) {
+    static long Callback(float volume, long arg2, long strAddr, uint8_t arg4, uint8_t arg5, uint8_t arg6) {
+        if (unkPtr == 0) {
+            unkPtr = arg2;
+        }
+        return Orig(volume, arg2, strAddr, arg4, arg5, arg6);
+    }
+};
+
 HOOK_DEFINE_TRAMPOLINE(AudioSystem__PlaySoundEffect) {
     static long Callback(float arg1, float arg2, long strAddr, long arg4) {
         return Orig(arg1, arg2, strAddr, arg4);
@@ -8,11 +18,21 @@ HOOK_DEFINE_TRAMPOLINE(AudioSystem__PlaySoundEffect) {
 };
 
 namespace AudioSystem {
+    void PlayBGM(std::string bgm) {
+        if (unkPtr == 0) {
+            Logging.Log("Cannot play BGM due to missing pointer!");
+        }
+        else {
+            AudioSystem__PlayBGM::Callback(1.0f, unkPtr, (long)&bgm, 1, 0, 0);
+        }
+    }
+
     void PlaySoundEffect(std::string soundEffect) {
         AudioSystem__PlaySoundEffect::Callback(1.0f, 1.0f, (long)&soundEffect, 0);
     }
 
     void InstallHooks() {
         AudioSystem__PlaySoundEffect::InstallAtOffset(0xa57e0);
+        AudioSystem__PlayBGM::InstallAtOffset(0x9fd60);
     }
 }
--- source/program/Hooks/Common/audio.hpp
+++ source/program/Hooks/Common/audio.hpp
@@ -1,6 +1,7 @@
 #include <string>
 
 namespace AudioSystem {
+    void PlayBGM(std::string bgm);
     void PlaySoundEffect(std::string soundEffect);
     void InstallHooks();
 }
