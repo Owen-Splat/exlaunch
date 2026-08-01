df6107ce6677b9e4d607e7aee584414e2763d320
Owen_Splat <Owen32Smart@gmail.com>
2026-08-01 09:40:06 -0400

    update rtld

--- source/lib/init/crt0.s
+++ source/lib/init/crt0.s
@@ -16,8 +16,12 @@
 
 .global __module_start
 __module_start:
+    // newer rtld versions check the first four bytes to determine the version
+    // 0, `b #0x8` (arm), or `b #0x8` (aarch64) are treated as the older version anything else is treated as the new version
+    // older versions of rtld appear to not care about what comes after the module header offset
     b entrypoint
     .word __nx_mod0 - __module_start
+    .word __sdk_version - __module_start
 
     .align 4
     .ascii "~~exlaunch uwu~~"
@@ -87,9 +91,22 @@ bss_loop:
 .align 2
 __nx_mod0:
     .ascii "MOD0"
-    .word  __dynamic_start__        - __nx_mod0
-    .word  __bss_start__            - __nx_mod0
-    .word  __bss_end__              - __nx_mod0
-    .word  __eh_frame_hdr_start__   - __nx_mod0
-    .word  __eh_frame_hdr_end__     - __nx_mod0
-    .word  exl_nx_module_runtime    - __nx_mod0
+    .word  __dynamic_start__            - __nx_mod0
+    .word  __bss_start__                - __nx_mod0
+    .word  __bss_end__                  - __nx_mod0
+    .word  __eh_frame_hdr_start__       - __nx_mod0
+    .word  __eh_frame_hdr_end__         - __nx_mod0
+    .word  exl_nx_module_runtime        - __nx_mod0
+    // these are only present in newer header versions, but newer sdk versions may check these so we should include them
+    .word   __relro_start__             - __nx_mod0
+    .word   __full_relro_end__          - __nx_mod0
+    .word   __module_name_start__       - __nx_mod0
+    .word   __module_name_end__         - __nx_mod0
+    .word   __note_gnu_build_id_start__ - __nx_mod0
+    .word   __note_gnu_build_id_end__   - __nx_mod0
+
+// this will only be checked if something throws an exception
+__sdk_version:
+    .word   20
+    .word   5
+    .word   6
